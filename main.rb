7f8ab3829d5b53fe0c99db74eb116557dd753f56
madacol <madacol10@gmail.com>
2018-06-25 21:52:49 -0500

    v1
    - filtered list by feedback, trade count and volume
    - Added VEF/BTC: buy, avg, sell
    - Added VEF/USD: buy, avg, sell

--- main.rb
+++ main.rb
@@ -1,5 +1,56 @@
 require 'sinatra'
+require 'open-uri'
+require 'json'
+
+MIN_FEEDBACK_SCORE = 99
+MIN_TRADE_COUNT = 1000
+MIN_BTC_VOLUME = 0.1
 
 get '/' do
-  "Hello World!"
+
+  buy_list_raw = JSON.parse open('https://localbitcoins.com/buy-bitcoins-online/vef/.json').read
+  buy_list = buy_list_raw['data']['ad_list'].reject do |tx|
+    trade_count = tx['data']['profile']['trade_count'].tr(' +','').to_i
+    feedback_score = tx['data']['profile']['feedback_score'].to_i
+    trade_count < MIN_TRADE_COUNT or feedback_score < MIN_FEEDBACK_SCORE
+  end
+  btc_volume = 0.0
+  vef_btc_buy_price = buy_list.each do |tx|
+    fiat_available = tx['data']['max_amount_available'].to_f
+    price = tx['data']['temp_price'].to_f
+    btc_volume = btc_volume + fiat_available / price
+    break price if btc_volume > MIN_BTC_VOLUME
+  end
+
+  sell_list_raw = JSON.parse open('https://localbitcoins.com/sell-bitcoins-online/vef/.json').read
+  sell_list = sell_list_raw['data']['ad_list'].reject do |tx|
+    trade_count = tx['data']['profile']['trade_count'].tr(' +','').to_i
+    feedback_score = tx['data']['profile']['feedback_score'].to_i
+    trade_count < MIN_TRADE_COUNT or feedback_score > MIN_FEEDBACK_SCORE
+  end
+  btc_volume = 0.0
+  vef_btc_sell_price = sell_list.each do |tx|
+    fiat_available = tx['data']['max_amount_available'].to_f
+    price = tx['data']['temp_price'].to_f
+    btc_volume = btc_volume + fiat_available / price
+    break price if btc_volume > MIN_BTC_VOLUME
+  end
+
+  usd_btc_avg_price = JSON.parse( open("https://apiv2.bitcoinaverage.com/constants/exchangerates/global").read )['rates']['BTC']['rate'].to_f
+
+  vef_btc_avg_price = (vef_btc_buy_price + vef_btc_sell_price) / 2
+  vef_usd_avg_price = vef_btc_avg_price * usd_btc_avg_price
+  vef_usd_buy_price = vef_btc_buy_price * usd_btc_avg_price
+  vef_usd_sell_price = vef_btc_sell_price * usd_btc_avg_price
+
+  JSON.unparse({
+                  "VEF/USD" => {buy: vef_usd_buy_price, avg: vef_usd_avg_price, sell: vef_usd_sell_price },
+                  "VEF/BTC" => {buy: vef_btc_buy_price, avg: vef_btc_avg_price, sell: vef_btc_sell_price },
+                  "list"    => {buy_list: buy_list, sell_list: sell_list}
+              })
+
+end
+
+after do
+  response.headers['Content-Type'] = 'application/json'
 end

c4d39eefd13ac4ddd8444c1cf98e36888dede83a
madacol <madacol10@gmail.com>
2018-06-28 00:16:55 -0500

    Now can fetch next pages from API
    Increased min volume to 0.5
    Sorted lists
    Added verbosity to server's output

--- main.rb
+++ main.rb
@@ -4,37 +4,47 @@ require 'json'
 
 MIN_FEEDBACK_SCORE = 99
 MIN_TRADE_COUNT = 100
-MIN_BTC_VOLUME = 0.1
+MIN_BTC_VOLUME = 0.5
+
+ASCENDING_ORDER = false
+DESCENDING_ORDER = true
+
+def getPrice(url, descending_order)
+  tx_list = []
+  price = 0
+  btc_volume = 0.0
+  while url and btc_volume < MIN_BTC_VOLUME
+    p "Fecthing: #{url}"
+    list_raw = JSON.parse open(url).read
+    p 'Done!'
+    url = list_raw['pagination']['next']
+    list_raw['data']['ad_list'].each do |tx|
+      trade_count = tx['data']['profile']['trade_count'].tr(' +','').to_i
+      feedback_score = tx['data']['profile']['feedback_score'].to_i
+      tx_list.push tx unless trade_count < MIN_TRADE_COUNT or feedback_score < MIN_FEEDBACK_SCORE
+    end
+    btc_volume = 0.0
+    tx_list.sort_by! {|tx| tx['data']['temp_price'].to_f}
+    tx_list.reverse! if descending_order
+    tx_list.each do |tx|
+      fiat_available = tx['data']['max_amount_available'].to_f
+      price = tx['data']['temp_price'].to_f
+      btc_volume += fiat_available / price
+      break unless btc_volume < MIN_BTC_VOLUME
+    end
+  end
+  return price
+end
 
 get '/' do
 
-  buy_list_raw = JSON.parse open('https://localbitcoins.com/buy-bitcoins-online/vef/.json').read
-  buy_list = buy_list_raw['data']['ad_list'].reject do |tx|
-    trade_count = tx['data']['profile']['trade_count'].tr(' +','').to_i
-    feedback_score = tx['data']['profile']['feedback_score'].to_i
-    trade_count < MIN_TRADE_COUNT or feedback_score < MIN_FEEDBACK_SCORE
-  end
-  btc_volume = 0.0
-  vef_btc_buy_price = buy_list.each do |tx|
-    fiat_available = tx['data']['max_amount_available'].to_f
-    price = tx['data']['temp_price'].to_f
-    btc_volume = btc_volume + fiat_available / price
-    break price if btc_volume > MIN_BTC_VOLUME
-  end
+  buy_list = []
+  sell_list = []
+  buy_list_url = 'https://localbitcoins.com/buy-bitcoins-online/vef/.json'
+  sell_list_url = 'https://localbitcoins.com/sell-bitcoins-online/vef/.json'
 
-  sell_list_raw = JSON.parse open('https://localbitcoins.com/sell-bitcoins-online/vef/.json').read
-  sell_list = sell_list_raw['data']['ad_list'].reject do |tx|
-    trade_count = tx['data']['profile']['trade_count'].tr(' +','').to_i
-    feedback_score = tx['data']['profile']['feedback_score'].to_i
-    trade_count < MIN_TRADE_COUNT or feedback_score > MIN_FEEDBACK_SCORE
-  end
-  btc_volume = 0.0
-  vef_btc_sell_price = sell_list.each do |tx|
-    fiat_available = tx['data']['max_amount_available'].to_f
-    price = tx['data']['temp_price'].to_f
-    btc_volume = btc_volume + fiat_available / price
-    break price if btc_volume > MIN_BTC_VOLUME
-  end
+  vef_btc_buy_price = getPrice( buy_list_url, ASCENDING_ORDER )
+  vef_btc_sell_price = getPrice( sell_list_url, DESCENDING_ORDER )
 
   usd_btc_avg_price = 1 / JSON.parse( open("https://apiv2.bitcoinaverage.com/constants/exchangerates/global").read )['rates']['BTC']['rate'].to_f
 

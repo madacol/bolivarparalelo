f777e1cfd33c6955af326afca47657e8fbc9149b
madacol <madacol10@gmail.com>
2018-06-30 12:30:35 -0500

    Added avg_1h ticker
    Gather 1btc of sample, and get price at 0.5btc volume
    Abstracted url lists to work for any currency
    Improved disclaimer
    Downsized fonts

--- main.rb
+++ main.rb
@@ -4,16 +4,21 @@ require 'json'
 
 MIN_FEEDBACK_SCORE = 99
 MIN_TRADE_COUNT = 100
-MIN_BTC_VOLUME = 0.5
+MIN_BTC_VOLUME_TO_SAMPLE = 1
+MIN_BTC_VOLUME_TO_GET_PRICE = 0.5
 
 ASCENDING_ORDER = false
 DESCENDING_ORDER = true
+
+def localbitcoins_buy_list_url(currency) "https://localbitcoins.com/buy-bitcoins-online/#{currency}/.json" end
+def localbitcoins_sell_list_url(currency) "https://localbitcoins.com/sell-bitcoins-online/#{currency}/.json" end
+localbitcoins_bitcoinaverage_url = "https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/"
 
 def getPrice(url, descending_order)
   tx_list = []
   price = 0
   btc_volume = 0.0
-  while url and btc_volume < MIN_BTC_VOLUME
+  while url and btc_volume < MIN_BTC_VOLUME_TO_SAMPLE
     p "Fecthing: #{url}"
     list_raw = JSON.parse open(url).read
     p 'Done!'
@@ -30,26 +35,33 @@ def getPrice(url, descending_order)
       fiat_available = tx['data']['max_amount_available'].to_f
       price = tx['data']['temp_price'].to_f
       btc_volume += fiat_available / price
-      break unless btc_volume < MIN_BTC_VOLUME
+      break unless btc_volume < MIN_BTC_VOLUME_TO_SAMPLE
     end
   end
-  return price
+  btc_volume = 0.0
+  tx_list.each do |tx|
+    fiat_available = tx['data']['max_amount_available'].to_f
+    price = tx['data']['temp_price'].to_f
+    btc_volume += fiat_available / price
+    return price unless btc_volume < MIN_BTC_VOLUME_TO_GET_PRICE
+  end
 end
 
 get '/' do
 
   buy_list = []
   sell_list = []
-  buy_list_url = 'https://localbitcoins.com/buy-bitcoins-online/vef/.json'
-  sell_list_url = 'https://localbitcoins.com/sell-bitcoins-online/vef/.json'
+  bitcoinaverage_usd_rates_url = 'https://apiv2.bitcoinaverage.com/constants/exchangerates/global'
 
-  vef_btc_buy_price = getPrice( buy_list_url, ASCENDING_ORDER )
-  vef_btc_sell_price = getPrice( sell_list_url, DESCENDING_ORDER )
+  vef_btc_buy_price = getPrice( localbitcoins_buy_list_url('vef'), ASCENDING_ORDER )
+  vef_btc_sell_price = getPrice( localbitcoins_sell_list_url('vef'), DESCENDING_ORDER )
+  vef_btc_avg_1h_price = JSON.parse( open(localbitcoins_bitcoinaverage_url).read )['VEF']['avg_1h'].to_f
 
-  usd_btc_avg_price = 1 / JSON.parse( open("https://apiv2.bitcoinaverage.com/constants/exchangerates/global").read )['rates']['BTC']['rate'].to_f
+  usd_btc_avg_price = 1 / JSON.parse( open(bitcoinaverage_usd_rates_url).read )['rates']['BTC']['rate'].to_f
 
   vef_btc_avg_price = (vef_btc_buy_price + vef_btc_sell_price) / 2
   vef_usd_avg_price = vef_btc_avg_price / usd_btc_avg_price
+  vef_usd_avg_1h_price = vef_btc_avg_1h_price / usd_btc_avg_price
   vef_usd_buy_price = vef_btc_buy_price / usd_btc_avg_price
   vef_usd_sell_price = vef_btc_sell_price / usd_btc_avg_price
 
@@ -57,7 +69,7 @@ get '/' do
     "VEFUSD" => {
       numerator: "VEF",
       denominator: "USD",
-      rates: { buy: vef_usd_buy_price, avg: vef_usd_avg_price, sell: vef_usd_sell_price },
+      rates: { buy: vef_usd_buy_price, avg: vef_usd_avg_price, sell: vef_usd_sell_price, avg_1h: vef_usd_avg_1h_price },
     },
     "VEFBTC" => {
       numerator: "VEF",

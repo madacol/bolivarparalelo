5e7ec337f1a30bcb2c1dde37d457b78f89086699
madacol <madacol10@gmail.com>
2018-09-13 22:14:44 -0500

    Major update to backend
    - Extracted keywords from ad's description
    - Use ads with at least 1 whitelisted keyword
    - Divided lots of code into it's own functions
    - Set maximum page to load to 3 instead of achieving min btc volume
    - Can now require this file into irb without executing, to test functions separately

--- bin/getPrices.rb
+++ bin/getPrices.rb
@@ -2,80 +2,171 @@ require 'open-uri'
 require 'json'
 require 'sinatra/activerecord'
 require './models'
+require 'i18n'
 
 MIN_FEEDBACK_SCORE = 99
 MIN_TRADE_COUNT = 100
-MIN_BTC_VOLUME_TO_SAMPLE = 1
 MIN_BTC_VOLUME_TO_GET_PRICE = 0.5
-
+MAX_PAGES_TO_SAMPLE = 3
 ASCENDING_ORDER = false
 DESCENDING_ORDER = true
+BITCOINAVERAGE_USD_RATES_URL = "https://apiv2.bitcoinaverage.com/constants/exchangerates/global"
+LOCALBITCOINS_BITCOINAVERAGE_URL = "https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/"
+def localbitcoins_ad_list_url(ticker, buyOrSell) "https://localbitcoins.com/#{buyOrSell}-bitcoins-online/#{ticker}/.json" end
 
-def localbitcoins_buy_list_url(currency) "https://localbitcoins.com/buy-bitcoins-online/#{currency}/.json" end
-def localbitcoins_sell_list_url(currency) "https://localbitcoins.com/sell-bitcoins-online/#{currency}/.json" end
-localbitcoins_bitcoinaverage_url = "https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/"
-bitcoinaverage_usd_rates_url = "https://apiv2.bitcoinaverage.com/constants/exchangerates/global"
+KEYWORDS_TRANSLATIONS =
+{
+  "ves" =>
+  {
+    "baneco"          => "banesco",
+    "bbva"            => "provincial",
+    "provicial"       => "provincial",
+    "provical"        => "provincial",
+    "mercantin"       => "mercantil",
+    "bdv"             => "venezuela",
+    "occidental"      => "bod"
+  }
+}
 
-def getPrice(url, sort_order)
-  tx_list = []
+KEYWORDS_WHITELIST =
+{
+  "ves" =>
+  [
+    "banesco",
+    "provincial",
+    "mercantil",
+    "venezuela",
+    "bicentenario",
+    "bod",
+    "bancaribe",
+    "banplus",
+    "bfc",
+    "bnc"
+  ]
+}
+
+
+def getHighReputationAds(ad_list)
+  ad_list.reject! do |ad|
+    trade_count = ad['data']['profile']['trade_count'].tr(' +','').to_i
+    feedback_score = ad['data']['profile']['feedback_score'].to_i
+    (trade_count < MIN_TRADE_COUNT or feedback_score < MIN_FEEDBACK_SCORE)
+  end
+  return ad_list
+end
+
+def addKeywords(ad_list, ticker)
+  return ad_list.collect do |ad|
+    payment_method = ad['data']['bank_name']
+    payment_method_normalized = I18n.transliterate payment_method.downcase
+    regex1 = "[^a-zA-Z0-9_.,]+"    # matches all characters that are not: a-zA-Z0-9_.,   | "a-z" translates to abcdefghij...
+    regex2 = "(?<=\\D)[.,]"        # matches any , or . that is preceded by a non-digit
+    regex  = /#{regex1}|#{regex2}/ # matches regex1 or regex2
+    payment_method_keywords_raw = payment_method_normalized.split(regex)
+    if KEYWORDS_TRANSLATIONS.has_key? ticker
+      payment_method_keywords_raw.collect! do |keyword|
+        KEYWORDS_TRANSLATIONS[ticker].has_key?( keyword )  ?  KEYWORDS_TRANSLATIONS[ticker][keyword]  :  keyword
+      end
+    end
+    payment_method_keywords = payment_method_keywords_raw.reject(&:empty?).uniq
+    ad['data']['keywords'] = payment_method_keywords
+    ad
+  end
+end
+
+def getWhitelistedAds(ad_list, ticker)
+  if KEYWORDS_WHITELIST.has_key? ticker
+    return ad_list.reject do |ad|
+      not ad['data']['keywords'].any? do |keyword|
+        KEYWORDS_WHITELIST[ticker].include? keyword
+      end
+    end
+  else return ad_list
+  end
+end
+
+def getNeededParams(ad_list)
+  return ad_list.collect do |ad|
+    {
+      "data" =>
+      {
+        "bank_name"            => ad['data']['bank_name'],
+        "temp_price"           => ad['data']['temp_price'],
+        "max_amount_available" => ad['data']['max_amount_available'],
+        "profile" =>
+        {
+          "trade_count"    => ad['data']['profile']['trade_count'],
+          "feedback_score" => ad['data']['profile']['feedback_score']
+        }
+      }
+    }
+  end
+end
+
+def getAdsFromUrl(url)
+  ad_list = []
+  for i in 1..MAX_PAGES_TO_SAMPLE
+    p "    Fecthing: #{url}"
+    list = JSON.parse open(url).read
+    p '    Done!'
+    url = list['pagination'].nil? ? nil : list['pagination']['next']
+    ad_list_raw = list['data']['ad_list']
+    ad_list_sanitized = getHighReputationAds( getNeededParams(ad_list_raw) )
+    ad_list.concat ad_list_sanitized
+    break if url.nil?
+  end
+  return ad_list
+end
+
+def getLobitPrice(ticker, buyOrSell)
+  url = localbitcoins_ad_list_url(ticker, buyOrSell)
+  p '  Loading sources'
+  ad_list = getAdsFromUrl(url)
+  p '  Done!'
+  ad_list.sort_by! {|ad| ad['data']['temp_price'].to_f}
+  ad_list.reverse! if buyOrSell == 'sell'
+  ad_list = addKeywords(ad_list, ticker)
+  ad_list = getWhitelistedAds( ad_list, ticker )
   price = 0
   btc_volume = 0.0
-  while url and btc_volume < MIN_BTC_VOLUME_TO_SAMPLE
-    p "  Fecthing: #{url}"
-    list_raw = JSON.parse open(url).read
-    p '  Done!'
-    url = list_raw['pagination']['next']
-    list_raw['data']['ad_list'].each do |tx|
-      trade_count = tx['data']['profile']['trade_count'].tr(' +','').to_i
-      feedback_score = tx['data']['profile']['feedback_score'].to_i
-      tx_list.push tx unless trade_count < MIN_TRADE_COUNT or feedback_score < MIN_FEEDBACK_SCORE
-    end
-    btc_volume = 0.0
-    tx_list.sort_by! {|tx| tx['data']['temp_price'].to_f}
-    tx_list.reverse! if sort_order == DESCENDING_ORDER
-    tx_list.each do |tx|
-      fiat_available = tx['data']['max_amount_available'].to_f
-      price = tx['data']['temp_price'].to_f
-      btc_volume += fiat_available / price
-      break unless btc_volume < MIN_BTC_VOLUME_TO_SAMPLE
-    end
-  end
-  btc_volume = 0.0
-  tx_list.each do |tx|
-    fiat_available = tx['data']['max_amount_available'].to_f
-    price = tx['data']['temp_price'].to_f
+  ad_list.each do |ad|      # return price when minimum volume is reached
+    fiat_available = ad['data']['max_amount_available'].to_f
+    price = ad['data']['temp_price'].to_f
     btc_volume += fiat_available / price
     return price if btc_volume >= MIN_BTC_VOLUME_TO_GET_PRICE
   end
-  p '  Warning: minimum volume not achieved'
+  p '  Warning: minimum volume-price not achieved'
   return price
 end
 
-bolivar_ticker = 'ves'
-p 'Getting prices'
-ves_btc_buy_price = getPrice( localbitcoins_buy_list_url(bolivar_ticker), ASCENDING_ORDER )
-ves_btc_sell_price = getPrice( localbitcoins_sell_list_url(bolivar_ticker), DESCENDING_ORDER )
-ves_btc_avg_1h_price = JSON.parse( open(localbitcoins_bitcoinaverage_url).read )[bolivar_ticker.upcase]['avg_1h'].to_f
-usd_btc_avg_price = 1 / JSON.parse( open(bitcoinaverage_usd_rates_url).read )['rates']['BTC']['rate'].to_f
 
-ves_btc_avg_price = (ves_btc_buy_price + ves_btc_sell_price) / 2
-ves_usd_avg_price = ves_btc_avg_price / usd_btc_avg_price
-ves_usd_avg_1h_price = ves_btc_avg_1h_price / usd_btc_avg_price
-ves_usd_buy_price = ves_btc_buy_price / usd_btc_avg_price
-ves_usd_sell_price = ves_btc_sell_price / usd_btc_avg_price
-p 'Done!'
+def getLobitPrices(ticker)
+  p 'Getting buy price'
+  buy = getLobitPrice(ticker, 'buy')
+  p 'Getting sell price'
+  sell = getLobitPrice(ticker, 'sell')
+  p 'Getting avg_1h price'
+  avg_1h = JSON.parse( open(LOCALBITCOINS_BITCOINAVERAGE_URL).read )[ticker.upcase]['avg_1h'].to_f
+  p 'Done!'
+  return { buy:  buy, sell: sell, avg_1h: avg_1h }
+end
 
-p 'Saving to Database'
+if __FILE__ == $0
+  bolivar_ticker = 'ves'
+  ves_btc_prices = getLobitPrices bolivar_ticker
+  usd_btc_avg_price = 1 / JSON.parse( open(BITCOINAVERAGE_USD_RATES_URL).read )['rates']['BTC']['rate'].to_f
 
-UsdBtc.create(
-  bitcoinaverage: usd_btc_avg_price,
-  datetime: Time.now
-)
-VesBtc.create(
-  buy:      ves_btc_buy_price,
-  sell:     ves_btc_sell_price,
-  avg_1h:   ves_btc_avg_1h_price,
-  datetime: Time.now
-)
-
-p 'Done!'
+  p ''
+  p 'Saving to Database'
+  UsdBtc.create(
+    bitcoinaverage: usd_btc_avg_price,
+    datetime:       Time.now
+  )
+  VesBtc.create(
+    buy:      ves_btc_prices[:buy],
+    sell:     ves_btc_prices[:sell],
+    avg_1h:   ves_btc_prices[:avg_1h],
+    datetime: Time.now
+  )
+  p 'Done!'
+end

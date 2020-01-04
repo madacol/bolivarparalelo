fcce5b0714cdcf49b3746b7fdec980fcd053c53c
Marco D'Agostini <madacol10@gmail.com>
2020-01-03 23:42:59 -0500

    Added new features to the beta front
    - Rates and Graphs can now be personalized by modifying the hash at the URL
    - That personalization can be bookmarked because it's in the URL
    
    Removed bitcoin rate temporarily

--- src/App.svelte
+++ src/App.svelte
@@ -1,128 +1,45 @@
 <script>
 	import { onMount } from 'svelte';
-
-	// STATES
-	let top_rate = {
-      numerator: "",
-      denominator: "",
-      rates: {
-        avg: ""
-      }
-    };
-	let bottom_rate = {
-      numerator: "",
-      denominator: "",
-      rates: {
-        avg: ""
-      }
-    };
-	let bitcoin_rate = {
-      rates: {
-        avg: ""
-      }
-	};
-	let chart_data = []
-	let time = "";
-	let calendar_text = 'Buscar fecha';
-
-
-	let canvas;
-	let chart;
-
-	function getHumanRate(rate) {
-		const num_digits = Math.log(Math.round(rate)) * Math.LOG10E + 1 | 0;  // https://stackoverflow.com/questions/14879691/get-number-of-digits-with-javascript
-		let human_rate;
-		if (num_digits < 4) {
-			const temp_scaling_factor = 10**(4-num_digits)
-			human_rate = Math.round( rate*temp_scaling_factor ) / temp_scaling_factor;
-		} else {
-			human_rate = Math.round(rate);
-		}
-		return human_rate.toLocaleString()
-	}
+	import Rate from './Rate.svelte';
 
 	moment.locale(navigator.language)
+	const _1Hms = 1000*3600;
+	//const _24Hms = _1Hms*24;
+	
+	let rates;
+	$: console.log(rates)
 
-	let handleSearchDate;
-	onMount(async () => {
+	const getRatesFromHash = () => {
+		rates =	window.location.hash.slice(1).split(';').map( rate => {
+					const [_counter_currency, _base_currency, range, _showRate, _showGraph] = rate.split(',')
+					const _end_date_time = range && Date.now();
+					const _date_time = range && (_end_date_time - Number(range)*_1Hms);
+					return {_counter_currency, _base_currency, _date_time, _end_date_time, _showRate, _showGraph}
+				})
+	}
 
-		// Getting and setting states
-		const response = await fetch("/api");
-		const json = await response.json();
-		top_rate = json.rates.primary;
-		bottom_rate = json.rates.secondary;
-		bitcoin_rate = json.rates.bitcoin;
-		chart_data = json.chart.data;
-		time = json.time;
+	window.location.hash = window.location.hash || `#ves,usd;ves,usd,${24*7},0;ves,eur`;
+	getRatesFromHash();
+	window.addEventListener('hashchange', getRatesFromHash);
 
-		const ctx = canvas.getContext('2d');
-		const data = {
-			datasets: [{
-				borderColor: '#003b68',
-				data: chart_data,
-			}],
-		}
+	const ratesToHash = rates => rates.map( rate => {
+		const {_counter_currency, _base_currency, _date_time, _end_date_time, _showRate, _showGraph} = rate;
+		const rateHash = [];
+		_counter_currency && rateHash.push(_counter_currency);
+		_base_currency && rateHash.push(_base_currency);
+		_date_time && _end_date_time && rateHash.push(Math.round(_date_time - _end_date_time)/_1Hms);
+		_showRate && rateHash.push(_showRate);
+		_showGraph && rateHash.push(_showGraph);
+		//console.log(rateHash);
+		return rateHash.join(',');
+	})	
+	const setRates = newRates => {
+		rates = newRates;
+		window.removeEventListener('hashchange', getRatesFromHash);
+		window.location.hash = ratesToHash(rates);
+		window.addEventListener('hashchange', getRatesFromHash);
+	}
 
-		chart = new Chart(ctx, {
-			// The type of chart we want to create
-			type: 'line',
-
-			// The data for our dataset
-			data: data,
-
-			// Configuration options go here
-			options: {
-				legend: {display: false},
-				maintainAspectRatio: false,
-				tooltips: {
-					mode: 'nearest',
-					intersect: false
-				},
-				scales: {
-					xAxes: [{
-						type: 'time',
-						time: {
-							displayFormats: {
-								hour: 'ddd hA',
-							},
-							tooltipFormat: 'MMM DD HH:mm',
-						},
-					}]
-				},
-				pan: {
-					enabled: true,
-					mode: 'x'
-				},
-				zoom: {
-					enabled: true,
-					mode: 'x',
-				},
-			}
-		});
-
-		handleSearchDate = async e => {
-			const DAY_MS = 1000*3600*24
-			const response = await fetch("/api/rate/ves/usd/time/"+String(e.target.valueAsNumber)+"/"+String(e.target.valueAsNumber+DAY_MS) );
-			const json = await response.json();
-			const rates = Object.values(json.rates)
-			let sum = 0;
-			const data = [];
-			rates.forEach(rate => {
-				const avg = parseFloat( rate.avg )
-				const timestamp = rate.unix_time_ms
-				sum += avg;
-				data.push ({
-					"x": timestamp,
-					"y": avg.toFixed(2),
-				});
-			})
-			const avg = sum / Object.values(rates).length;
-			top_rate.rates.avg = getHumanRate(avg);
-			chart.data.datasets[0].data = data;
-			chart.resetZoom();
-			calendar_text = e.target.value;
-		}
-	});
 </script>
 
 <div class="pos-f-t">
@@ -133,19 +50,19 @@
 		</div>
 	</div>
 	<nav class="navbar navbar-expand-lg navbar-dark">
-		<a class="navbar-brand">Bolívar Paralelo</a>
+		<a href="/" class="navbar-brand">Bolívar Paralelo</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="justify-content-end collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav">
-			<li class="nav-item">
-			<input type="date" id="get_date_history" on:change={handleSearchDate}>
-			<label class="btn btn-outline-dark" for="get_date_history">
-				<span id="calendar_text">{calendar_text} </span>
-				<img alt="calendar icon" src="/icons/calendar_icon_white.png" width="25" height="25">
-			</label>
-			</li>
+			<!-- <li class="nav-item">
+				<input type="date" id="get_date_history" on:change={handleSearchDate}>
+				<label class="btn btn-outline-dark" for="get_date_history">
+					<span id="calendar_text">{calendar_text} </span>
+					<img alt="calendar icon" src="/icons/calendar_icon_white.png" width="25" height="25">
+				</label>
+			</li> -->
 			<li class="nav-item px-2">
 			</li>
 		</ul>
@@ -154,35 +71,14 @@
 
 
 <div id="body" class="px-2">
-
-	<div class="d-flex justify-content-center update-time py-1">
-		<div class="px-2">Actualizado hace {time}</div>
-	</div>
-
-	<div id="content" class="d-flex justify-content-center flex-wrap">
-
-		<div class="d-flex justify-content-center flex-wrap flex-row">
-			<div class="align-self-center"><div class="monto-label">        {top_rate.denominator}:</div></div>
-			<div class="align-self-center"><div class="monto px-2 px-md-4" id="top-monto"> {top_rate.rates.avg}</div></div>
-			<div class="align-self-center"><div class="monto-label">        {top_rate.numerator}</div></div>
-		</div>
-
-		<div class="chart-container" style="position: relative; width: 100%">
-			<canvas bind:this={canvas} height="150"></canvas>
-		</div>
-
-		<div class="d-flex justify-content-center flex-wrap flex-row">
-			<div class="align-self-center"><div class="monto-label">        {bottom_rate.denominator}:</div></div>
-			<div class="align-self-center"><div class="monto px-2 px-md-4"> {bottom_rate.rates.avg}</div></div>
-			<div class="align-self-center"><div class="monto-label">        {bottom_rate.numerator}</div></div>
-		</div>
-
-	</div>
+	{#each rates as rateOptions (JSON.stringify(rateOptions))}
+		<Rate {...rateOptions} />
+	{/each}
 </div>
 
 <nav class="navbar d-flex justify-content-between fixed-bottom">
 	<div></div>
-	<a class="navbar-brand">Bitcoin: {bitcoin_rate.rates.avg} $</a>
+	<!-- <a class="navbar-brand">Bitcoin: {bitcoin_rate.rates.avg} $</a> -->
 	<div>
 		<a href="https://twitter.com/bolivarparalel0">
 			<img alt="twitter icon" src="/icons/Twitter_Social_Icon_Circle_White.png" width="40" height="40">
@@ -192,3 +88,17 @@
 		</a>
 	</div>
 </nav>
+
+<style>
+	nav{
+		background-color: #003b68;
+	}
+	#body {
+		display: flex;
+		flex-direction: column;
+		padding-bottom: 50px;
+	}
+	#navbarSupportedContent ul li {
+		position: relative;
+	}
+</style>
---
+++ src/Rate.svelte
@@ -0,0 +1,211 @@
+<script>
+
+	import { onMount } from 'svelte';
+	import { RANGE } from './Constants.js'
+
+	// Props
+	export let _counter_currency;
+	export let _base_currency;
+	export let _date_time;
+	export let _end_date_time;
+	export let _showRate;
+	export let _showGraph;
+
+	// States
+	let counter_currency     = _counter_currency;
+	let base_currency   = _base_currency;
+	let date_time     = _date_time;
+	let end_date_time = _end_date_time;
+	let showRate = _showRate;
+	let showGraph = _showGraph;
+	let history = [];
+	$: fetching_data = fetchData(counter_currency, base_currency, date_time, end_date_time);
+	let updated_time = "";
+
+	const getQueryUrl = (counter_currency, base_currency, date_time, end_date_time) => {
+		let url = `/api/rate/${counter_currency.toLowerCase()}/${base_currency.toLowerCase()}`
+		if (!date_time)    return url
+
+		url += `/time/${date_time}`;
+		if (!end_date_time)    return url
+
+		url += `/${end_date_time}`;
+		return url;
+	}
+
+	function getHumanRate(rate) {
+		const num_digits = Math.log(Math.round(rate)) * Math.LOG10E + 1 | 0;  // https://stackoverflow.com/questions/14879691/get-number-of-digits-with-javascript
+		let human_rate;
+		if (num_digits < 4) {
+			const temp_scaling_factor = 10**(4-num_digits)
+			human_rate = Math.round( rate*temp_scaling_factor ) / temp_scaling_factor;
+		} else {
+			human_rate = Math.round(rate);
+		}
+		return human_rate.toLocaleString()
+	}
+
+	const fetchData = async (counter_currency, base_currency, date_time, end_date_time) => {
+		const url = getQueryUrl(counter_currency, base_currency, date_time, end_date_time);
+		const response = await fetch(url);
+		const json = await response.json();
+		const rates = Object.values(  (date_time) ? json.rates : [json]  )
+		let sum = 0;
+		const chart_data = [];
+		rates.forEach(rate => {
+			const avg = parseFloat( rate.avg )
+			const timestamp = rate.unix_time_ms
+			sum += avg;
+			chart_data.push ({
+				"x": timestamp,
+				"y": avg.toFixed(2),
+			});
+		})
+		const avg = sum / rates.length;
+		const rate = getHumanRate(avg);
+		// const last_timestamp = rates[rates.length-1].unix_time_ms
+		// search_text = e.target.value;
+		history = chart_data;
+		return rate
+	}
+
+	const canvasMounted = (canvas, history) => {
+		let ctx = canvas.getContext('2d');
+
+		const height = canvas.height;
+
+		let chart = new Chart(ctx, {
+			// The type of chart we want to create
+			type: 'line',
+
+			// The data for our dataset
+			data: {
+				datasets: [{
+					borderColor: '#003b68',
+					data: history,
+				}],
+			},
+
+			// Configuration options go here
+			options: {
+				legend: {display: false},
+				maintainAspectRatio: false,
+				tooltips: {
+					mode: 'nearest',
+					intersect: false
+				},
+				scales: {
+					xAxes: [{
+						type: 'time',
+						time: {
+							displayFormats: {
+								hour: 'ddd hA',
+							},
+							tooltipFormat: 'MMM DD HH:mm',
+						},
+					}]
+				},
+				pan: {
+					enabled: true,
+					mode: 'x'
+				},
+				zoom: {
+					enabled: true,
+					mode: 'x',
+				},
+			}
+		});
+
+		// hack to keep height. For some reason after initialiazing chart, it forgets it.  --_(=/)_--
+		canvas.height=height;
+		canvas.style.height=`${height}px`;
+
+		return {
+			update(history) {
+				chart.data.datasets[0].data = history;
+				chart.resetZoom();
+			}
+		}
+
+	}
+
+</script>
+
+<rateContainer class="container">
+	{#await fetching_data}
+		<!-- promise is pending -->
+		<p>Cargando...</p>
+	{:then rate}
+		<!-- promise was fulfilled -->
+		{#if updated_time}
+			<div class="d-flex justify-content-center update-time py-1">
+				<div class="px-2">Actualizado hace {updated_time}</div>
+			</div>
+		{/if}
+
+		<div class="content d-flex justify-content-center flex-wrap">
+
+			{#if showRate !== "0"}
+				<div class="d-flex justify-content-center flex-wrap flex-row">
+					<div class="align-self-center"><div class="monto-label">        {base_currency}:</div></div>
+					<div class="align-self-center"><div class="monto px-2 px-md-4" id="top-monto"> {rate}</div></div>
+					<div class="align-self-center"><div class="monto-label">        {counter_currency}</div></div>
+				</div>
+			{/if}
+			{#if showGraph !== "0" && history.length > 2}
+				<div class="chart-container" style="position: relative; width: 100%">
+					<canvas use:canvasMounted={history} height="150"></canvas>
+				</div>
+			{/if}
+		</div>
+	{/await}
+</rateContainer>
+
+<style>
+	rateContainer {
+		align-self: center;
+		text-align: center;
+		border-bottom: #ffffff0a 4px solid;
+		padding: 10px;
+	}
+	canvas {
+		height: 150px;
+	}
+	.monto {
+		font-size: calc(30px + 6vw);
+		line-height: 1.15;
+	}
+	.monto-label {
+		font-size: calc(10px + 2vw);
+		line-height: 1.15;
+	}
+	.update-time {
+		font-size: calc(10px + 1vw);
+		line-height: 1.15;
+	}
+	.btn-outline-dark {
+		border-color: white;
+		color: white;
+	}
+	#get_date_history {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		opacity: 0;
+	}
+	#get_date_history::-webkit-calendar-picker-indicator {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+	}
+	#get_date_history:hover + label {
+		background-color: black;
+	}
+	label {
+		margin: 0;
+	}
+	.content {
+		flex-direction: column;
+		/* min-height: 70vh; */
+	}
+</style>

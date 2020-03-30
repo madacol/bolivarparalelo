ad5e91e1e1c6c3eebe09efc57fd17bcedd9ffe4e
Marco D'Agostini <madacol10@gmail.com>
2020-03-30 15:59:01 -0500

    RadioButton: radio inputs that look like buttons, and all the text looks selected

---
+++ src/RadioButton.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let value;
+    export let label;
+    export let group;
+</script>
+
+<label>
+    <input type="radio" bind:group {value}>
+    <span>{label}</span>
+</label>
+
+<style>
+    input {
+        display: none;
+    }
+    span {
+        display: inline-block;
+        cursor: pointer;
+        padding: 0.3em 0.6em;
+        margin: 0.4em;
+        font-size: 1em;
+        border-radius: 0.5em;
+        border: 1px solid var(--gray3);
+    }
+    input:checked + span {
+        background-color: var(--navBackgroundColor);
+        border-color: var(--navBackgroundColor);
+    }
+</style>

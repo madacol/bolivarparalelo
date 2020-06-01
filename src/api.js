09ac49e2e5113e43f2ff36f131a1e8236a5ad92e
Marco D'Agostini <madacol10@gmail.com>
2020-06-01 02:33:47 -0500

    API: implemented getting time ranges

--- src/api.js
+++ src/api.js
@@ -65,4 +65,78 @@ router.get('/rate/:counter_currency_code/:base_currency_code', async (req, res) 
     });
 })
 
+
+async function getTimeRangeRates(currency_code, start, end) {
+    // Get currency data
+    const { rows: currencies } = await db.query(
+        'SELECT id, code, symbol, name, "namePlural", flag FROM currencies WHERE code = $1',
+        [currency_code]
+    );
+    if (currencies.length === 0) return null;
+
+    const [{id, ...currency}] = currencies;
+
+    // Get rates
+    const { rows: rates } = await db.query(
+        'SELECT buy, sell, created_at AS date FROM lobit_prices\
+            WHERE currency_id = $1\
+                AND (created_at, created_at)\
+                    OVERLAPS ($2, $3)\
+            ORDER BY created_at DESC\
+            LIMIT 10000',
+        [id, start, end]
+    );
+    if (rates.length === 0) return null;
+    return {rates, currency};
+}
+
+router.get('/rate/:counter_currency_code/:base_currency_code/time/:start_str/:end_str?', async (req, res) => {
+    const { counter_currency_code, base_currency_code, start_str, end_str } = req.params;
+    const start = new Date(Number(start_str) || start_str); // Try to cast to a number (unix time), if it fails fallback to string
+    const end = new Date(
+        (end_str &&
+            ( Number(end_str) || end_str ) // Try to cast to a number (unix time), if it fails fallback to string
+        ) || (start.getTime() + 60*60*1000) // If no `end_str`, set `end` to 1 hour after `start`
+    );
+    const counterPromise = getTimeRangeRates(counter_currency_code, start, end);
+    let basePromise;
+    if (base_currency_code !== 'btc') basePromise = getTimeRangeRates(base_currency_code, start, end);
+
+    const counterCurrency = await counterPromise;
+    if (!counterCurrency) return res.status(404).send(`Currency ${counter_currency_code} not found`);
+    const counter_rates_hourstamped = {};
+    counterCurrency.rates.forEach( rate => {
+        const hourstamp = `${rate.date.toLocaleDateString()} ${rate.date.getHours()}`
+        counter_rates_hourstamped[hourstamp] = rate;
+    })
+
+    if ((base_currency_code === 'btc')) {
+        return res.send({
+            counter_currency: counterCurrency.currency,
+            base_currency: BITCOIN,
+            rates: counter_rates_hourstamped,
+        })
+    }
+
+    const baseCurrency = await basePromise;
+    if (!baseCurrency) return res.status(404).send(`Currency ${base_currency_code} not found`);
+    const rates_hourstamped = {};
+    baseCurrency.rates.forEach( base_btc => {
+        const hourstamp = `${base_btc.date.toLocaleDateString()} ${base_btc.date.getHours()}`
+        const counter_btc = counter_rates_hourstamped[hourstamp];
+        if (!counter_btc) return;
+        rates_hourstamped[hourstamp] = {
+            buy: counter_btc.buy / base_btc.sell,
+            sell: counter_btc.sell / base_btc.buy,
+            date: counter_btc.date
+        };
+    })
+
+    res.send({
+        counter_currency: counterCurrency.currency,
+        base_currency: baseCurrency.currency,
+        rates: rates_hourstamped,
+    })
+})
+
 export default router;

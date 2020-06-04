2c55cdba07066dbae816350885f8d87501223b1c
Marco D'Agostini <madacol10@gmail.com>
2020-06-04 11:37:50 -0500

    vercel-sapper wasn't working when declaring middlewares in a single statement

--- src/server.js
+++ src/server.js
@@ -4,21 +4,21 @@ import morgan from 'morgan';
 import * as sapper from '@sapper/server';
 import api from './api';
 
-const app = express()
-	.use(
+const app = express();
+app.use(
 		morgan('dev'),
-		compression(),
-		express.static('static'),
-	)
-	.use('/api', api)
-	.use(sapper.middleware({
+	compression(),
+	express.static('static'),
+);
+app.use('/api', api)
+app.use(sapper.middleware({
 			session: (req, res) => ({
 				// user: req.user,
 				// lang: 'es'
-			})
-		}))
-	.listen(process.env.PORT, err => {
+		})
+	}))
+app.listen(process.env.PORT, err => {
 		if (err) console.log('error', err);
-	});
+});
 
 export default app;

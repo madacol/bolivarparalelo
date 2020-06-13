0cd65efce8b0ffa9bc5105e0c85e0d7cd3be1e3e
Marco D'Agostini <madacol10@gmail.com>
2020-06-13 02:57:32 +0000

    new helper: layoutEncoding: to encode a decode the layout in and out of a Cookie

---
+++ src/helpers/layoutEncoding.js
@@ -0,0 +1,107 @@
+import { _1H_in_ms } from "../CONSTANTS";
+
+/**
+ * Normalize functions to encode/decode base64
+ * 
+ * Browsers uses `atob`/`btoa` functions
+ * But nodeJS uses `Buffer.from`
+ */
+const b64ToAscii = (typeof atob === 'function') ? atob : base64 => Buffer.from(base64, 'base64').toString('ascii');
+const asciiToB64 = (typeof btoa === 'function') ? btoa : string => Buffer.from(string).toString('base64');
+
+/**
+ * Decodes a base64 string into a layout-config-object
+ * @param {String} layoutString - A version+base64 string. E.g. `"1vdmVzLHVzZCwxO2NvcCxidGMsMTt1c2QsYnRjXzAsMTY4O3VzZCxidGNfMCwxNjgsMg=="`
+ * 
+ * @returns {Object} layout, E.g.
+ * NOTE: if example string is decoded again, `start` and `end` won't match since they are encoded as the number of hours in the past relative to current time
+ * ```js
+ *   [
+ *      {params: {counter_currency_code: "ves", base_currency_code: "usd", timeRange: undefined}, showType: 0, showBuySell: true},
+ *      {params: {counter_currency_code: "cop", base_currency_code: "btc", timeRange: undefined}, showType: 0, showBuySell: true},
+ *      {params: {counter_currency_code: "usd", base_currency_code: "btc", timeRange: {start: 1591407769315, end: 1592012569315}}, showType: 0, showBuySell: false},
+ *      {
+ *          params: {
+ *              counter_currency_code: "usd",
+ *              base_currency_code: "btc",
+ *              timeRange: {
+ *                  start: 1591407769315,
+ *                  end: 1592012569315
+ *              }
+ *          },
+ *          showType: 2,
+ *          showBuySell: false
+ *      }
+ *   ]
+ * ```
+ */
+export function decodeLayout(layoutString) {
+
+    const [version, base64String] = layoutString.split(/v(.+)/);
+    const layout = b64ToAscii(base64String).split(';').map( rateString => {
+        const [configs, timeRangeConfigs] = rateString.split('_');
+        const [counter_currency_code, base_currency_code, showBuySell] = configs.split(',');
+        const isTimeRangeEnabled = (typeof timeRangeConfigs === "string") && timeRangeConfigs.length > 1;
+        let timeRange, showType = 0;
+        if (isTimeRangeEnabled) {
+            const [start_hourRange_str, hourRange_str, showType_str] = (timeRangeConfigs || '').split(',');
+            const start_hourRange = Number(start_hourRange_str);
+            const hourRange = Number(hourRange_str);
+            if (Number.isInteger(start_hourRange) && Number.isInteger(hourRange)) {
+                const end = Date.now() - start_hourRange*_1H_in_ms;
+                const start = end - hourRange*_1H_in_ms;
+                timeRange = { start, end };
+                if (showType_str) showType = Number(showType_str);
+            } else {
+                console.error('timeRange is invalid');
+                console.error('timeRangeConfigs:', timeRangeConfigs)
+            }
+        }
+        const rate = {
+            params: {
+                counter_currency_code,
+                base_currency_code,
+                timeRange,
+            },
+            showType,
+            showBuySell: (showBuySell === '1'),
+        };
+
+        return rate;
+    })
+
+    return layout;
+}
+
+/**
+ * Encodes a layout-config-object into a base64 string.
+ * See sibling function `decode()` for more information
+ * @param {Object} layout 
+ */
+export function encodeLayout(layout) {
+
+    const layoutStringList = layout.map(rate => {
+        const { params: {counter_currency_code, base_currency_code, timeRange}, showBuySell, showType } = rate;
+
+        const allConfigs = [];
+        {
+            const configs = [counter_currency_code, base_currency_code];
+            if (showBuySell) configs.push('1');
+            allConfigs.push(configs.join(','));
+        }
+        if (timeRange) {
+            const {start, end} = timeRange;
+            const start_hourRange = Math.round((Date.now() - end) / _1H_in_ms);
+            const hourRange = Math.round((end - start) / _1H_in_ms);
+            const timeRangeConfigs = [start_hourRange, hourRange];
+            if (showType > 0) timeRangeConfigs.push(showType);
+            allConfigs.push(timeRangeConfigs.join(','));
+        }
+        rateString = allConfigs.join('_');
+
+        return rateString;
+    })
+    const layoutString = asciiToB64(layoutStringList.join(';'));
+
+    return '1v'+layoutString;
+}

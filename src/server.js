f4e7829236e0881e5eff1a8058994fc2d10ab67d
Marco D'Agostini <madacol10@gmail.com>
2020-06-13 20:46:02 -0500

    server: pass cookie to the `preload()` in the session parameter

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import compression from 'compression';
 import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 import * as sapper from '@sapper/server';
 import api from './api';
 
@@ -8,15 +9,15 @@ const app = express();
 app.use(
 		morgan('dev'),
 	compression(),
+	cookieParser(),
 	express.static('static'),
 );
 app.use('/api', api)
 app.use(sapper.middleware({
-			session: (req, res) => ({
-				// user: req.user,
-				// lang: 'es'
-		})
-	}))
+	session: (req, res) => ({
+		layout: req.query.layout || req.cookies.layout,
+	})
+}))
 app.listen(process.env.PORT, err => {
 		if (err) console.log('error', err);
 });

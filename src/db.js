1a91302e8f92ca82fe004403c1dd1936364b8762
Marco D'Agostini <madacol10@gmail.com>
2020-06-24 02:25:27 +0000

    Configure DB resources consumption
    - Reduced number of clients from `10` to `6`
    - Increased idle timeout from 10s to 30s
    - Added a timeout to connection and query

--- src/db.js
+++ src/db.js
@@ -1,7 +1,11 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    query_timeout: 10000,
+    max: 6,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 5000,
 })
 
 export default pool

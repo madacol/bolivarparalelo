c61182ea0f951b7af58d4a58c6b11db93fe9ba8c
Marco D'Agostini <madacol10@gmail.com>
2020-06-30 21:01:10 +0000

    Reorganized middlewares so they are invoked in the correct order
    morgan now uses the 'combined' profile in production

--- src/server.js
+++ src/server.js
@@ -4,25 +4,28 @@ import cookieParser from 'cookie-parser';
 import * as sapper from '@sapper/server';
 import api from './api';
 
+
 const app = express();
-if (process.env.NODE_ENV === 'development') {
+
+const dev = process.env.NODE_ENV === 'development';
+app.use(morgan(dev ? 'dev' : 'combined'))
+if (dev) {
 	app.use('/flags', express.static('node_modules/flag-icon-css/flags/4x3'))
+	app.use(express.static('static'))
 }
+app.use('/api', api);
 app.use(
-	morgan('dev'),
 	cookieParser(),
-	express.static('static'),
-);
-app.use('/api', api)
-app.use(sapper.middleware({
-	session: (req, res) => ({
-		layouts: {
-			query: req.query.layout,
-			cookie: req.cookies.layout,
-		},
-		langs: req.acceptsLanguages(),
+	sapper.middleware({
+		session: (req, res) => ({
+			layouts: {
+				query: req.query.layout,
+				cookie: req.cookies.layout,
+			},
+			langs: req.acceptsLanguages(),
+		})
 	})
-}))
+)
 app.listen(process.env.PORT, err => {
 	if (err) console.log('error', err);
 });
